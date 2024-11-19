40e17c68558d40c95dddc03121b8b2abed7ceb71
LuckGets <kasdies12@gmail.com>
2024-11-19 16:54:39 +0700

    add:secret

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Yo!</p>
+        <p>Hi there. HAHA!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
